5115de3c8ebe77c99aed6c11b1536000ca5fbe6c
diogenispanagiotis <diogenis.panagiotis@gmail.com>
2021-04-10 21:28:34 -0400

    update schema

--- pages/index.js
+++ pages/index.js
@@ -26,73 +26,21 @@ async function saveUser(user) {
   return await response.json();
 }
 
+// renderSignUp = () => {
+
+// }
+
 export default function Home({ initialUsers }) {
   const [users, setUsers] = useState(initialUsers);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-        
-        {users.map((user, index) => (
-          <div className={styles.grid} key={index}>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
-          </div>
-        ))}
-        
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div className="container mx-auto m-20">
+		<div className="grid grid-cols-1 rounded-md bg-gray-50">
+			{users.map((user, index) => (
+				<div className="m-5" key={index}>
+					<pre>{JSON.stringify(user, null, 2)}</pre>
+				</div>
+			))}
+		</div>
     </div>
   )
 }
