52c8250aa16e8114f1dcf450728e3d8aaa7e6ced
Raj Ashish Desai <rajdesai94@gmail.com>
2021-04-15 22:28:41 -0400

    use server for db funcs

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,7 +1,3 @@
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
 export default async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
@@ -9,20 +5,38 @@ export default async (req, res) => {
 
   try {
     const user = JSON.parse(req.body);
-    const users = await prisma.user.findMany({
-      where: {
-        username: user.username,
-        platform: user.platform,
-        phoneNumber: user.phoneNumber,
-      }
-    });
-    if (users.length > 0) {
-      throw new Error('User already exists!');
+    const response = await fetch(`${process.env.EXPRESS_URL}/signup/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+
+    if (response.status === '409') {
+      throw new Error('User already exists')
     }
-    const savedUser = await prisma.user.create({ data: user });
-    res.status(200).json(savedUser);
+    res.status(201).send(response.body);
   } catch (err) {
     console.log(err)
     res.status(400).json({ message: 'Something went wrong' });
   }
+  // try {
+  //   const user = JSON.parse(req.body);
+  //   const users = await prisma.user.findMany({
+  //     where: {
+  //       username: user.username,
+  //       platform: user.platform,
+  //       phoneNumber: user.phoneNumber,
+  //     }
+  //   });
+  //   if (users.length > 0) {
+  //     throw new Error('User already exists!');
+  //   }
+  //   const savedUser = await prisma.user.create({ data: user });
+  //   res.status(200).json(savedUser);
+  // } catch (err) {
+  //   console.log(err)
+  //   res.status(400).json({ message: 'Something went wrong' });
+  // }
 };
